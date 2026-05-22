ba0b68771187f5e6a3b03c68a632e5cb85864131
iz <43661631+mencretsu@users.noreply.github.com>
2026-05-22 17:34:49 +0700

    Update formatter.py

--- formatter.py
+++ formatter.py
@@ -10,12 +10,12 @@ def format_message(data: dict) -> str:
 
     if similar_names:
         msg += f"\n\n⚠️ Found {len(similar_names)} similar name ({name})"
-        for ca in similar_names:
-            msg += f"\n┗ <code>{ca}</code>"
-
+        for s in similar_names:
+            msg += f"\n┗ ${s['symbol']} • <code>{s['mint']}</code>"
+    
     if similar_ticks:
         msg += f"\n\n⚠️ Found {len(similar_ticks)} similar tick (${symbol})"
-        for ca in similar_ticks:
-            msg += f"\n┗ <code>{ca}</code>"
+        for s in similar_ticks:
+            msg += f"\n┗ ${s['symbol']} • <code>{s['mint']}</code>"
 
     return msg
