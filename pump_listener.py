bd4a2ae9eb3b805c5532f202595cc52c5f0f5efa
iz <43661631+mencretsu@users.noreply.github.com>
2026-05-22 12:54:14 +0700

    Update pump_listener.py

--- pump_listener.py
+++ pump_listener.py
@@ -7,8 +7,11 @@ PUMP_WS = "wss://pumpportal.fun/api/data"
 async def listen_new_tokens(callback):
     while True:
         try:
-            async with websockets.connect(PUMP_WS) as ws:
-                # Subscribe ke event token baru yang bonding subscribeNewToken, subscribeMigration
+            async with websockets.connect(
+                PUMP_WS,
+                ping_interval=20,
+                ping_timeout=10
+            ) as ws:
                 payload = {
                     "method": "subscribeMigration"
                 }
@@ -17,8 +20,8 @@ async def listen_new_tokens(callback):
 
                 async for message in ws:
                     data = json.loads(message)
-                    if data.get("txType") == "create":
-                        await callback(data)
+                    await callback(data)
+
         except Exception as e:
             print(f"WS Error: {e}, reconnecting in 5s...")
             await asyncio.sleep(5)
